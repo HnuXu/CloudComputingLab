d0db3c64f9b42cfdf49b4c83d22bae4a53fdd416
squirrel <52901465+Jockey-Fish@users.noreply.github.com>
2021-04-07 16:54:47 +0800

    Update main.cc
    修改了输出函数display

--- Lab1/src/main.cc
+++ Lab1/src/main.cc
@@ -9,14 +9,16 @@
 #include <sys/sysinfo.h>
 
 #include "sudoku.h"
-#define PTHREAD_NUM 4 //线程数目为4
+#define PTHREAD_NUM 5 //线程数目为4
 char puzzle[tasks][128];//存储数独
+char answer[tasks][128];//存储答案
 int total = 0;
 int fin=0;//当前数独的序号
 bool finish=false;//是否解决
 
 pthread_t pt[PTHREAD_NUM];//线程号
 pthread_mutex_t mutex;//🔒
+
 
 int getWorkID()
 {
@@ -36,8 +38,29 @@ int getWorkID()
 
 void *Run(void *args)
 {
-
+    bool (*solve)(int,int[],int,int[])=solve_sudoku_basic;//使用basic算法
+    int board[N];
+    int spaces[N];
+    while (!finish)
+    {
+        int id=getWorkID();
+        if (id==-1)
+            break;
+        int t=input(puzzle[id],board,spaces);
+        if (solve(0,board,t,spaces))
+        {
+            if (!solved(board))
+                assert(0);
+            for (int i=0;i<N;i++)//保存结果
+                answer[id][i] = char('0' + board[i]);
+        }
+        else
+        {
+            printf("No: %d,无解\n", id);
+        }
+    }
 }
+
 
 void Create()//创建线程
 {
@@ -49,6 +72,12 @@ void Create()//创建线程
     }
 }
 
+void End()
+{
+    for (int i = 0; i < PTHREAD_NUM; i++)
+        pthread_join(pt[i], NULL);//pthread_join用来等待一个线程的结束
+}
+
 int64_t now()//⏲
 {
   struct timeval tv;
@@ -56,40 +85,41 @@ int64_t now()//⏲
   return tv.tv_sec * 1000000 + tv.tv_usec;
 }
 
+void display()
+{
+    FILE* fw = fopen("answer.txt","w");
+    for(int i=0;i<total; i++)
+        fprintf(fw,"%s\n",answer[i]);
+}
+
 int main(int argc, char* argv[])
 {
   init_neighbors();
 
   FILE* fp = fopen(argv[1], "r");
-  bool (*solve)(int) = solve_sudoku_basic;
-  if (argv[2] != NULL)
+  bool (*solve)(int,int[],int,int[])=solve_sudoku_basic;//使用basic算法
+  /*if (argv[2] != NULL)
     if (argv[2][0] == 'a')
       solve = solve_sudoku_min_arity;
     else if (argv[2][0] == 'c')
       solve = solve_sudoku_min_arity_cache;
     else if (argv[2][0] == 'd')
-      solve = solve_sudoku_dancing_links;
+      solve = solve_sudoku_dancing_links; */
   int64_t start = now();//⏲
 
   Create();
-  /*while (fgets(puzzle, sizeof puzzle, fp) != NULL) {
-    if (strlen(puzzle) >= N) {
-      ++total;
-      input(puzzle);
-      init_cache();
-      //if (solve_sudoku_min_arity_cache(0)) {
-      //if (solve_sudoku_min_arity(0))
-      //if (solve_sudoku_basic(0)) {
-      if (solve(0)) {
-        ++total_solved;
-        if (!solved())
-          assert(0);
-      }
-      else {
-        printf("No: %s", puzzle);
-      }
+  while (fgets(puzzle[total], sizeof puzzle, fp) != NULL)
+    {
+        if (strlen(puzzle[total]) >= N)
+        {
+            pthread_mutex_lock(&mutex);
+            total++;
+            pthread_mutex_unlock(&mutex);
+        }
     }
-  }*/
+  End();//结束线程
+  //输出答案
+  display();
   int64_t end = now();
   double sec = (end-start)/1000000.0;
   printf("时间:\n");
