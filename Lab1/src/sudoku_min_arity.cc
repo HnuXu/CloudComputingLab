be6c0dd93b480ac6f8c4c31d47426312d817ff5c
squirrel <52901465+Jockey-Fish@users.noreply.github.com>
2021-04-08 21:33:41 +0800

    Update sudoku_min_arity.cc
    尝试用MINA算法来跑多线程

--- Lab1/src/sudoku_min_arity.cc
+++ Lab1/src/sudoku_min_arity.cc
@@ -4,7 +4,7 @@
 
 #include "sudoku.h"
 
-static int arity(int cell)
+static int arity(int cell,int *board)//修改参数
 {
   bool occupied[10] = {false};
   for (int i = 0; i < NEIGHBOR; ++i) {
@@ -14,14 +14,14 @@ static int arity(int cell)
   return std::count(occupied+1, occupied+10, false);
 }
 
-static void find_min_arity(int space)
+static void find_min_arity(int space,int *spaces,int *board)//修改参数 传递指针
 {
   int cell = spaces[space];
   int min_space = space;
-  int min_arity = arity(cell);
+  int min_arity = arity(cell,board);
 
   for (int sp = space+1; sp < nspaces && min_arity > 1; ++sp) {
-    int cur_arity = arity(spaces[sp]);
+    int cur_arity = arity(spaces[sp],board);
     if (cur_arity < min_arity) {
       min_arity = cur_arity;
       min_space = sp;
@@ -31,25 +31,27 @@ static void find_min_arity(int space)
   if (space != min_space) {
     std::swap(spaces[min_space], spaces[space]);
   }
+  //return spaces;
 }
 
-bool solve_sudoku_min_arity(int which_space)
+bool solve_sudoku_min_arity(int which_space,int *board,int nspaces,int *spaces)//修改参数
 {
   if (which_space >= nspaces) {
     return true;
   }
 
-  find_min_arity(which_space);
+  find_min_arity(which_space,spaces,board);
+
   int cell = spaces[which_space];
 
   for (int guess = 1; guess <= NUM; ++guess) {
-    if (available(guess, cell)) {
+    if (available(guess,cell,board)) {
       // hold
       assert(board[cell] == 0);
       board[cell] = guess;
 
       // try
-      if (solve_sudoku_min_arity(which_space+1)) {
+      if (solve_sudoku_min_arity(which_space+1,board,nspaces,spaces)) {//修改递归参数
         return true;
       }
 
